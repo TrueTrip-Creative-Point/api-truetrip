3876a08eb1a43a42670ed7f65bfebaef9a2139f4
Yordy-code81 <U201923959@upc.edu.pe>
2021-11-05 16:39:33 -0500

    feat: infraestructure implemented with application

--- src/travelers/infraestructure/persitence/typeorm/entities/traveler.id.typeorm.ts
+++ src/travelers/infraestructure/persitence/typeorm/entities/traveler.id.typeorm.ts
@@ -1,1 +1,14 @@
-//TO-DO: traveler.id.typeorm
+import { PrimaryGeneratedColumn } from 'typeorm';
+
+export class TravelerIdTypeORM {
+  @PrimaryGeneratedColumn('increment', { type: 'bigint', name: 'id', unsigned: true })
+  public value: number;
+
+  private constructor(value: number) {
+    this.value = value;
+  }
+
+  public static from(value: number): TravelerIdTypeORM  {
+    return new TravelerIdTypeORM(value);
+  }
+}
